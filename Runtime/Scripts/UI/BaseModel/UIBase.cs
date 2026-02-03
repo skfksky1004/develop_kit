3918963fdfb0465cb54938a43e1b66d78c16bf06
YunJuSeok <tktls13@gmail.com>
2026-02-03 11:13:15 +0900

    BasePanel과 BasePopup을 UIBase로 취합

--- Runtime/Scripts/UI/BaseModel/UIBase.cs
+++ Runtime/Scripts/UI/BaseModel/UIBase.cs
@@ -1,23 +1,24 @@
+using System;
 using UnityEngine;
 
 namespace skfksky1004.DevKit.UI
 {
-    public abstract class UIBasePanel : MonoBehaviour
+    public abstract class UIBase : MonoBehaviour
     {
-        private CanvasGroup canvasGroup;
+        private CanvasGroup _canvasGroup;
 
         protected RectTransform RectTransform => (RectTransform)transform;
 
         private void Start()
         {
-            if (canvasGroup is null)
-                canvasGroup = GetComponent<CanvasGroup>();
+            if (_canvasGroup is null)
+                _canvasGroup = GetComponent<CanvasGroup>();
         }
 
         protected void SetPanelAlpha(float value)
         {
-            if (canvasGroup != null)
-                canvasGroup.alpha = value;
+            if (_canvasGroup != null)
+                _canvasGroup.alpha = value;
         }
 
         public void SetActive(bool bActive, GameObject targetGo = null)
@@ -27,38 +28,26 @@ namespace skfksky1004.DevKit.UI
 
             targetGo?.SetActive(bActive);
         }
-
+        
         /// <summary>
         /// 패널 생성후 Initialize
         /// </summary>
-        public abstract void CreatedPanel();
+        public abstract void Created();
 
         /// <summary>
         /// 패널 열때
         /// </summary>
-        public abstract void ShowPanel();
+        public abstract void Show();
 
         /// <summary>
         /// 패널 닫을때
         /// </summary>
-        public abstract void HidePanel();
+        public abstract void Hide();
 
         /// <summary>
         /// esc 반응
         /// </summary>
         /// <returns></returns>
         public abstract bool IsProcessEscape();
-
-        /// <summary>
-        /// 패널 네임
-        /// </summary>
-        /// <returns></returns>
-        public abstract string GetPanelName();
-
-        // /// <summary>
-        // /// 
-        // /// </summary>
-        // /// <returns></returns>
-        // public abstract UIType GetPanelType();
     }
 }
